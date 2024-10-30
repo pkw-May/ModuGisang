627c2964237a4099ca257faf19a116c466a80a24
won0845 <tpqms775@gmail.com>
2024-10-31 00:44:21 +0900

    feat : 요청 헤더 민감 정보 **** 표시 후 로깅

--- backend/src/common/filters/all-exceptions.filter.ts
+++ backend/src/common/filters/all-exceptions.filter.ts
@@ -21,6 +21,10 @@ export class AllExceptionsFilter implements ExceptionFilter {
     const message =
       exception instanceof HttpException ? exception.getResponse() : exception;
 
+    // 민감 정보 필터링 적용
+    const filteredBody = this.filterSensitiveInfo(request.body);
+    const filteredHeaders = this.filterSensitiveInfo(request.headers);
+
     this.logger.error(
       `Http Status: ${status} Error Message: ${JSON.stringify(message)}`,
       exception instanceof Error ? exception.stack : '',
@@ -29,8 +33,8 @@ export class AllExceptionsFilter implements ExceptionFilter {
     // 요청 관련 정보 로깅
     this.logger.log(`Request Method: ${request.method}`);
     this.logger.log(`Request URL: ${request.url}`);
-    this.logger.log(`Request Headers: ${JSON.stringify(request.headers)}`);
-    this.logger.log(`Request Body: ${JSON.stringify(request.body)}`);
+    this.logger.log(`Request Headers: ${JSON.stringify(filteredHeaders)}`);
+    this.logger.log(`Request Body: ${JSON.stringify(filteredBody)}`);
 
     // 예외 응답 전송
     response.status(status).json({
@@ -38,5 +42,18 @@ export class AllExceptionsFilter implements ExceptionFilter {
       timestamp: new Date().toISOString(),
       path: request.url,
     });
+  }
+
+  filterSensitiveInfo(data: any) {
+    const fieldsToMask = ['password', 'token', 'cookie'];
+    const maskedData = { ...data };
+
+    for (const field of fieldsToMask) {
+      if (maskedData[field]) {
+        maskedData[field] = '****'; // 민감 정보 마스킹
+      }
+    }
+
+    return maskedData;
   }
 }
